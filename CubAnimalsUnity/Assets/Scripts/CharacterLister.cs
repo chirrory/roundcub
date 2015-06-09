c44a3b7c4c61781879b393ca1983be416d7baff6
chirrory <aino.markkinen@hotmail.com>
2015-06-09 16:11:05 +0300

    Base for character list and renamed some classes

---
+++ CubAnimalsUnity/Assets/Scripts/CharacterLister.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterLister : MonoBehaviour
+{
+    public List<Character> Characters;
+}
+
+[Serializable]
+public class Character
+{
+    public string Name;
+    public GameObject Body;
+}
--- CubAnimalsUnity/Assets/Scripts/CharacterMovement.cs
+++ CubAnimalsUnity/Assets/Scripts/CharacterMovement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
+public class CharacterMovement : MonoBehaviour {
 
     Rigidbody2D rb;
     bool jumping;
