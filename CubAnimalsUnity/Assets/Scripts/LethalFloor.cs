7c56946223a2acc31963d3159c37e14b97b616e4
Laboratorium <neirwith@hotmail.com>
2015-06-10 12:33:41 +0300

    Added test scene and lethal component

---
+++ CubAnimalsUnity/Assets/Scripts/LethalFloor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LethalFloor : MonoBehaviour {
+
+    void OnCollisionEnter2D()
+    {
+        Application.LoadLevel(Application.loadedLevelName);
+    }
+}
